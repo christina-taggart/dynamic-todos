b80323ad1bbd202929f1e5c111e31d7cdbad9083
Apprentice <apprentice@dbc15.local>
2014-02-05 17:24:05 -0800

    implemented drag and drop

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -53,5 +53,94 @@ $(document).ready(function() {
 
 
 
+
+
+
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+  // Target (this) element is the source node.
+  this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault(); // Necessary. Allows us to drop.
+  }
+
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  // this / e.target is the current hover target.
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');  // this / e.target is previous target element.
+}
+
+var tasks = document.querySelectorAll('li');
+[].forEach.call(tasks, function(task) {
+  task.addEventListener('dragstart', handleDragStart, false);
+  task.addEventListener('dragenter', handleDragEnter, false);
+  task.addEventListener('dragover', handleDragOver, false);
+  task.addEventListener('dragleave', handleDragLeave, false);
+});
+
+function handleDrop(e) {
+  // this / e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // stops the browser from redirecting.
+  }
+
+  // See the section on the DataTransfer object.
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+  this.style.opacity = '1';
+  [].forEach.call(tasks, function (task) {
+    task.classList.remove('over');
+  });
+}
+
+var tasks = document.querySelectorAll('li');
+[].forEach.call(tasks, function(task) {
+  task.addEventListener('dragstart', handleDragStart, false);
+  task.addEventListener('dragenter', handleDragEnter, false)
+  task.addEventListener('dragover', handleDragOver, false);
+  task.addEventListener('dragleave', handleDragLeave, false);
+  task.addEventListener('drop', handleDrop, false);
+  task.addEventListener('dragend', handleDragEnd, false);
+});
+
+function handleDrop(e) {
+  // this/e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
 });
 
