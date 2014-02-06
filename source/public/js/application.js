e0dfdfb937f82246eb9471a97ae3bac9f1a2ddc3
Elliot Chalfant & John Olmsted <pair+epchalfant.qsymmachus@devbootcamp.com>
2014-02-05 17:00:48 -0800

    Adds #addTodo and corresponding event handler

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -4,21 +4,30 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    $('.add').on('click', function(event) {
+      event.preventDefault();
+        addTodo($('.todo').val())
+    })
+
+
   }
+
 
   //Create functions to add, remove and complete todos
 
-
-
-  function buildTodo(todoName) {
+  var addTodo = function(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
     // Modifies it's text to use the passed in todoName.
     $todo.find('h2').text(todoName);
     // Returns the jQueryDOMElement to be used elsewhere.
-    return $todo;
+    $('.todo_list').append($todo);
   }
 
 
   bindEvents();
 });
+
+
+
+
