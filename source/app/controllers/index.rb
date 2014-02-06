f595b61fe1b986cedd567c232a54e74c76bdeb39
Apprentice <apprentice@dbc15.local>
2014-02-05 16:50:54 -0800

    made todo list functional with ajax

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,9 +1,39 @@
+require_relative '../../config/environment'
+
+before do
+	@tasks = Task.all
+end
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
-post '/add_todo' do
-  p "Inside /add_todo route!"
+post '/created' do
+	Task.create(params)
+	@task = Task.last
+	erb :created_task, :layout => false
 end
 
+delete '/:task_id/deleted' do
+	Task.find(params[:task_id]).destroy
+	@tasks = Task.all
+	params[:task_id]
+end
+
+put '/:task_id/edited' do
+	p params
+	@task = Task.find(params[:task_id])
+	@task.update_attributes(title: params[:title], content: params[:content])
+	p @task
+	erb :edited_task
+end
+
+get '/:task_id/edit' do
+	@task = Task.find(params[:task_id])
+	erb :edit_form, :layout => false
+end
+
+get '/:task_id/complete' do
+	Task.find(params[:task_id]).update_attributes(completed: 1)
+	redirect '/'
+end
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,24 +1,57 @@
 $(document).ready(function() {
-  var todoTemplate = $.trim($('#todo_template').html());
+  $('#create-task').on("submit", function(e){
+    e.preventDefault()
+    $.ajax({
+      type: 'post',
+      url: '/created',
+      data: $(this).serialize()
+    }).done(function(erb) {
+      $('#task-list').append(erb)
+    })
+  })
 
-  function bindEvents() {
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
-  }
+  $(document).on("click", '.delete-button', function(e){
+    e.preventDefault();
+    console.log("delete")
+    url = $(this).attr('href')
+    $.ajax({
+      type: 'delete',
+      url: url
+    }).done(function(task_id) {
+      $("#" + task_id).remove()
+    })
+  })
 
-  //Create functions to add, remove and complete todos
+  $(document).on("click", '.edit-button', function(e){
+    e.preventDefault();
+    url = $(this).attr('href')
+    $.ajax({
+      type: 'get',
+      url: url
+
+    }).done(function(erb) {
+      id = url.match(/\d+/)[0]
+      $("#edit-task").remove()
+      $("#" + id).append(erb)
+
+    })
+  })
+
+  $(document).on("submit", '#edit-task', function(e){
+    e.preventDefault();
+    url = $(this).attr('action')
+    $.ajax({
+      type: 'put',
+      url: url,
+      data: $(this).serialize()
+    }).done(function(erb) {
+      console.log("asdfasdf")
+      id = url.match(/\d+/)[0]
+      $("#" + id).replaceWith(erb)
+    })
+  })
 
 
 
-  function buildTodo(todoName) {
-    // Creates an jQueryDOMElement from the todoTemplate.
-    var $todo = $(todoTemplate);
-    // Modifies it's text to use the passed in todoName.
-    $todo.find('h2').text(todoName);
-    // Returns the jQueryDOMElement to be used elsewhere.
-    return $todo;
-  }
-
-
-  bindEvents();
 });
+
