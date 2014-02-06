0ddd96556444605778b7c9ed389a3155271d1117
Elliot Chalfant & John Olmsted <pair+epchalfant.qsymmachus@devbootcamp.com>
2014-02-05 17:24:50 -0800

    Fixed #addTodo & corresponding event handler; now AJAXified

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,9 +1,8 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  new_todo = Todo.create(params)
+  new_todo.todo_content
 end
-
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -4,24 +4,35 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
-    $('.add').on('click', function(event) {
+    $('.toolbox form').on('submit', function(event) {
       event.preventDefault();
-        addTodo($('.todo').val())
+      formData = $(this).serialize();
+      console.log(formData)
+        $.ajax({
+          type: this.method,
+          url: this.action,
+          data: formData
+        }).done(function(serverResponse) {
+          console.log(serverResponse)
+        })
     })
 
+    $('.delete').on('click', function(event) {
+      event.preventDefault();
 
+    })
   }
 
 
   //Create functions to add, remove and complete todos
 
-  var addTodo = function(todoName) {
+  var buildTodo = function(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
     // Modifies it's text to use the passed in todoName.
     $todo.find('h2').text(todoName);
     // Returns the jQueryDOMElement to be used elsewhere.
-    $('.todo_list').append($todo);
+    return $todo;
   }
 
 
