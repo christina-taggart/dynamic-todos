6e057a76f78e82122d9e7c54d1263835c82b8339
Christina Taggart & Alexander Leishman <pair+christina-taggart.leishman@devbootcamp.com>
2014-02-05 20:31:38 -0800

    Release 0

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,9 +1,21 @@
 get '/' do
   # Look in app/views/index.erb
+  @todos = Todo.all
   erb :index
 end
 
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  @todo = Todo.create(params)
+  erb :list_element, layout: false
+end
+
+put '/todos/:id' do
+  todo = Todo.find(params[:id])
+  todo.update_attributes(completed: true)
+end
+
+delete '/todos/:id' do
+  t = Todo.find(params[:id])
+  t.destroy
 end
 
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,24 +1,41 @@
 $(document).ready(function() {
+
   var todoTemplate = $.trim($('#todo_template').html());
 
   function bindEvents() {
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
+
+    $("#todo_form").submit(function(e){
+      console.log("test")
+      e.preventDefault();
+      var todo_text = $("#todo_field").val().trim();
+      $.post('/add_todo', {"todo_content" : todo_text}).done(function(response){
+        $(".todo_list").append(response);
+      });
+    })
+
+    $(".todo_list").on('click', '.delete', function(e){
+        var $this = $(this)
+        var todo_id = $this.data('id');
+        $.ajax({
+          url: 'todos/' + todo_id,
+          type: 'DELETE',
+        }).done(function(response) {
+          $this.closest('div').fadeOut();
+          $this.remove();
+        })
+    })
+
+    $(".todo_list").on('click', '.complete', function(e){
+        var $this = $(this)
+        var todo_id = $this.data('id');
+        $.ajax({
+          url: 'todos/' + todo_id,
+          type: 'put',
+        }).done(function(response) {
+          $this.closest('div').addClass('complete')
+        })
+    })
   }
-
-  //Create functions to add, remove and complete todos
-
-
-
-  function buildTodo(todoName) {
-    // Creates an jQueryDOMElement from the todoTemplate.
-    var $todo = $(todoTemplate);
-    // Modifies it's text to use the passed in todoName.
-    $todo.find('h2').text(todoName);
-    // Returns the jQueryDOMElement to be used elsewhere.
-    return $todo;
-  }
-
 
   bindEvents();
 });
