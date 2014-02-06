9610301f716dd337f51bb25b46581e2ead5cce94
Aditya Mahesh & Ricardo A. Rubio <pair+amahesh0501.rickarubio@devbootcamp.com>
2014-02-05 19:30:17 -0800

    Added delete and complete

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
   var todoTemplate = $.trim($('#todo_template').html());
 
   function bindEvents() {
@@ -12,13 +14,73 @@ $(document).ready(function() {
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
-    var $todo = $(todoTemplate);
+    var todo = $('#todo_template');
     // Modifies it's text to use the passed in todoName.
-    $todo.find('h2').text(todoName);
+    // var to_do_template = '<div class="todo"><h2><p>' + todoName + '</p></h2><ul><li><a href="#" id= "" class="delete">Delete</a></li><li><a href="#" class="complete">Complete</a></li></ul></div>'
+    todo.prepend(todoName)
+    console.log('prepending worked!');
+    // $todo.css('display', 'block');
     // Returns the jQueryDOMElement to be used elsewhere.
-    return $todo;
+    return todo;
+  }
+
+  function updateTodo(todoNode) {
+    $(todoNode).parent().parent().parent().addClass('completed-item');
+  }
+
+  function deleteTodo(todoNode) {
+    $(todoNode).parent().parent().parent().remove();
   }
 
 
+
+
   bindEvents();
-});
+
+  // Our code starts below here
+  $('#add_todo').on('submit', function(e) {
+    e.preventDefault();
+    var todo_creation_req = $.ajax({
+      type: 'POST',
+      url: '/todos',
+      data: $('#user_input').serialize()
+    });
+    todo_creation_req.done(function(todo){
+      buildTodo(todo);
+      //console.log(todo);
+    });
+    todo_creation_req.always(function() {
+      $('#user_input').val("");
+    })
+  })
+
+  // bind on delete link
+  $('#todo_template').on('click', '.delete', function(e) {
+    e.preventDefault();
+    var todoNode = this;
+    var todo_id = $(todoNode).data('id');
+    var todo_deletion_req = $.ajax({
+      type: 'DELETE',
+      url: '/todos/' + todo_id
+    });
+
+    todo_deletion_req.done(function(todo){
+      deleteTodo(todoNode);
+    });
+  });
+
+  // bind on complete link
+  $('#todo_template').on('click', '.complete', function(e) {
+    e.preventDefault();
+    var todoNode = this;
+    var todo_id = $(todoNode).data('id');
+    var options = {
+      type: 'PUT',
+      url: '/todos/' + todo_id
+    }
+
+    $.ajax(options).done( function(todo){
+        updateTodo(todoNode);
+    });
+  });
+}); // end of document.ready
