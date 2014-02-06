bc144a03271147118d068f5bde6097f4725d2719
Christina Taggart & Alexander Leishman <pair+christina-taggart.leishman@devbootcamp.com>
2014-02-05 22:00:38 -0800

    Added basic drag and drop functionality

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   function bindEvents() {
 
     $("#todo_form").submit(function(e){
-      console.log("test")
       e.preventDefault();
       var todo_text = $("#todo_field").val().trim();
       $.post('/add_todo', {"todo_content" : todo_text}).done(function(response){
@@ -37,5 +36,77 @@ $(document).ready(function() {
     })
   }
 
+  function handleDrags(){
+
+    // handle drag start
+    var dragSrcEl = null;
+
+    function handleDragStart(e) {
+      this.style.opacity = '0.5';
+
+      dragSrcEl = this;
+
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+
+    //handle drag over
+
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault(); //prevents bubbling up of actions from browser
+      }
+      e.dataTransfer.dropEffect = 'move'; //unsure of use. will learn later
+      return false;
+    }
+
+    function handleDragEnter(e) {
+      if(this.classList[0] == "todo"){
+        this.classList.add('over');
+      }
+    }
+
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+
+      if (e.stopPropagation) {
+        e.stopPropagation();
+      }
+
+      if (dragSrcEl != this){
+
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+        this.style.opacity = '1';
+      }
+
+      return false;
+    }
+
+    // handle drag end
+
+    function handleDragEnd(e) {
+      [].forEach.call(todos, function(todo){
+        todo.classList.remove('over');
+      })
+    }
+
+    var todos = document.querySelectorAll('.todo_list .todo'); // necessary?
+
+    [].forEach.call(todos, function(todo) {
+      todo.addEventListener('dragstart', handleDragStart, false);
+      todo.addEventListener('dragenter', handleDragEnter, false);
+      todo.addEventListener('dragover', handleDragOver, false);
+      todo.addEventListener('dragleave', handleDragLeave, false);
+      todo.addEventListener('drop', handleDrop, false);
+      todo.addEventListener('dragend', handleDragEnd, false);
+    });
+  }
+
+
   bindEvents();
+  handleDrags();
 });
