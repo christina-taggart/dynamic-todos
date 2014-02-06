cf046a3f73cf9b8739b5893ec4ce2c26712582d6
Kevin Yeh & Matthew Higgins <pair+kyeh.mh120888@devbootcamp.com>
2014-02-05 17:32:17 -0800

    WIP temporary commit to move to another workstation

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -5,5 +5,12 @@ end
 
 post '/add_todo' do
   p "Inside /add_todo route!"
+  p params
+  dog = {}
+
+  dog[:todo_content] = 'fake jello'
+  p "dog params: #{dog}"
+  @todo = Todo.create(dog)
+  @todo.todo_content.to_s
 end
 
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -4,6 +4,14 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    $('form').on('submit', function(event) {
+      event.preventDefault()
+      var send_todo = $( this ).serialize()
+      $.post( '/add_todo', send_todo )
+        .done( function(todo_text) {
+          alert('todo_text is: ' + todo_text)
+        })
+    })
   }
 
   //Create functions to add, remove and complete todos
@@ -13,7 +21,7 @@ $(document).ready(function() {
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
-    // Modifies it's text to use the passed in todoName.
+    // Modifies its text to use the passed in todoName.
     $todo.find('h2').text(todoName);
     // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
