39dd60064d5c664c09ecb8f141f1485b7ee1a2ed
Aditya Mahesh & Emmanuel Kaunitz <pair+amahesh0501.mechamanny@devbootcamp.com>
2014-02-11 01:58:04 -0800

    Updated challenge from last week with attempts to implement drag and drop and handlebars

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,12 +1,31 @@
 get '/' do
-  @todos = Todo.all
+  # Look in app/views/index.erb
+  redirect '/todos'
+end
+
+get '/todos' do
+  @todos = Todo.order(:created_at).reverse
   erb :index
 end
 
-post '/add_todo' do
-  p params
-  p "$$$$$$$"
-  @todo_to_add = Todo.create(todo_content: params[:item_added])
-  return @todo_to_add.todo_content
+
+post '/todos' do
+  @todo = Todo.create(params)
+  erb :_todo_template, layout: false
+
 end
+
+delete '/todos/:id' do
+  Todo.delete(params[:id])
+  "deleted"
+end
+
+put '/todos/:id' do
+  todo = Todo.find(params[:id])
+  current_val = todo.completed
+  current_val ? current_val = false : current_val = true
+  todo.update_attributes({completed: current_val})
+  "updated todo"
+end
+
 
